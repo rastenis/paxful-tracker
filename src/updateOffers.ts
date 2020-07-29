f34c8bcf528c38c74ee42ad3b43ee25f0ce27930
Matas Rastenis <matas2k@gmail.com>
2020-07-29 16:22:36 +0300

    fixes

--- src/updateOffers.ts
+++ src/updateOffers.ts
@@ -9,13 +9,11 @@ export async function updateOffer(
   offerDescription: any
 ): Promise<number> {
   // no margin range == offer control disabled
-  if (
-    !offerDescription.adjustedMarginMin &&
-    !offerDescription.adjustedMarginsMin[offers[0].denomination]
-  )
+  if (!offerDescription.adjustedMarginsMin?.[offers[0].denomination]) {
     return;
+  }
 
-  if(username == null) {
+  if (username == null) {
     const currentUser = await api.user.username();
     username = currentUser.data.username;
   }
@@ -30,7 +28,9 @@ export async function updateOffer(
       ? offerDescription.adjustedMarginMax
       : offerDescription.adjustedMarginsMax[offers[0].denomination];
 
-  const sortedOffers = offers.sort((offer1, offer2) => offer1.margin - offer2.margin);
+  const sortedOffers = offers.sort(
+    (offer1, offer2) => offer1.margin - offer2.margin
+  );
 
   // check if paymentMethod and denomination match
   const result = await api.offers.self();
@@ -47,7 +47,7 @@ export async function updateOffer(
       const fiatMax = parseFloat(element.fiat_amount_range_max);
 
       //////////////////////////
-      // GUESS: fiat_min is in interval [fiat_amount_range_min; fiat_amount_range_max] 
+      // GUESS: fiat_min is in interval [fiat_amount_range_min; fiat_amount_range_max]
       //////////////////////////
       if (denomNumber > fiatMax || denomNumber < fiatMin) {
         return;
@@ -57,13 +57,13 @@ export async function updateOffer(
     // find largest margin which would be first in the list and in the given range
     let newMargin: number;
 
-    for(let i = 0; i < sortedOffers.length; i++) {
-
+    for (let i = 0; i < sortedOffers.length; i++) {
       // exclude self posted offers from comparison
-      if(sortedOffers[i].offer_owner_username == username)
+      if (sortedOffers[i].offer_owner_username == username) {
         continue;
+      }
 
-      if(sortedOffers[i].margin >= marginMin ) {
+      if (sortedOffers[i].margin >= marginMin) {
         newMargin = Math.max(marginMin, offers[i].margin - 0.01);
         break;
       }
